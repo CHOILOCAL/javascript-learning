a654bbe32a2077900154fbdec59074f70ae79225
CHOILOCAL <giants1217@naver.com>
2022-01-10 17:20:20 +0900

    76/529

---
+++ bigyo.js
@@ -0,0 +1,17 @@
+console.log(5 == 5);
+console.log("5" == 5); // 동등 연산자 == (자료형 상관X)
+console.log('5' === 5); // 일치 연산자 === (자료형 상관O), 엄격한 기준
+console.log(5 != 1);
+console.log(5 != "1");
+console.log(5 != "5");
+console.log(5 === 5);
+console.log(5 !== 10);
+console.log(5 !== "5");
+console.log('-----------');
+console.log(5 > 3);
+console.log(5 < 3);
+console.log(5 <= 6);
+console.log(5 >= 5);
+console.log('-----------');
+console.log("Italy" > "America");
+console.log("Korea" < "korea");
